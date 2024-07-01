b31461df56ca82271df78b956c6c33cd6deb63c4
Nohan550 <nohanmuttakim.bd@gmail.com>
2024-07-01 21:51:51 +0600

    data rendered

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,65 @@
-// 75c82df7a5ce6827e14b42270f4008c6
-
+import axios from "axios";
+import { useEffect } from "react";
 import { useState } from "react";
-
-const ApiKey = "75c82df7a5ce6827e14b42270f4008c6";
-
+import "./index.css";
+import {
+  IoMdCloudy,
+  IoMdRainy,
+  IoMdSnow,
+  IoMdSunny,
+  IoMdThunderstorm,
+} from "react-icons/io";
+import { BsCloudDrizzleFill, BsCloudHaze2Fill } from "react-icons/bs";
 const App = () => {
   const [data, setData] = useState(null);
-  const [location, setLocation] = useState("Chittagong");
+  const [location, setLocation] = useState("Dhaka");
+
+  useEffect(() => {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${
+      import.meta.env.VITE_APIKEY
+    }`;
+    axios.get(url).then((res) => {
+      setData(res.data);
+    });
+  }, [location]);
+
+  // loading animation
+
+  if (!data) {
+    return <div className="loader"></div>;
+  }
+
+  // setting the icon according to weather
+
+  let icon;
+
+  switch (data.weather[0].main) {
+    case "Cloudy":
+      icon = <IoMdCloudy />;
+      break;
+    case "Haze":
+      icon = <BsCloudHaze2Fill />;
+      break;
+    case "Rain":
+      icon = <IoMdRainy />;
+      break;
+    case "Clear":
+      icon = <IoMdSunny />;
+      break;
+    case "Drizzle":
+      icon = <BsCloudDrizzleFill />;
+      break;
+    case "Snow":
+      icon = <IoMdSnow />;
+      break;
+    case "Thunderstorm":
+      icon = <IoMdThunderstorm />;
+      break;
+  }
+
   return (
     <>
-      <h1 className="text-3xl">Vite + React</h1>
+      <h1 className="text-6xl">{icon}</h1>
     </>
   );
 };
